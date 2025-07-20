0d10f46979fc9ca8c4c4592cae01e61826c6d17f
jedtherooster <55644921+jedtherooster@users.noreply.github.com>
2025-07-21 11:17:20 +1200

    Add speed increase upgrade

--- Gravity Flip/Assets/Scripts/ShopMenu.cs
+++ Gravity Flip/Assets/Scripts/ShopMenu.cs
@@ -3,5 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class ShopMenu : MonoBehaviour
 {
+    public PlayerMovement playerMovement;
+    public GameManager gameManager;
+
+    [Header("Costs")] 
+    public int speedIncreaseCost = 2;
     
+    public void speedIncrease()
+    {
+        if (gameManager.playerCredits >= speedIncreaseCost)
+        {
+            gameManager.playerCredits -= speedIncreaseCost;
+            gameManager.playerCreditsText.text = gameManager.playerCredits.ToString();
+        
+            playerMovement.moveSpeed += 1;
+        }
+    }
 }
