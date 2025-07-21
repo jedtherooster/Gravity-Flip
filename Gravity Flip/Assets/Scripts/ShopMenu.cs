45eb81a58c45782d98a3d2e76906351be6b43034
jedtherooster <55644921+jedtherooster@users.noreply.github.com>
2025-07-22 10:40:29 +1200

    Add brighter spotlight upgrade

--- Gravity Flip/Assets/Scripts/ShopMenu.cs
+++ Gravity Flip/Assets/Scripts/ShopMenu.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
+    [Header("Scripts")]
     public PlayerMovement playerMovement;
     public GameManager gameManager;
+    public Light2D playerSpotlight;
 
     [Header("Costs")] 
     public int speedIncreaseCost = 2;
     public int heightIncreaseCost = 4;
     public int brighterSpotLightCost = 1;
-    public int disableGroundCheckCost = 10;
+    public int widerFovCost = 3;
+    
+    [Header("Panels")]
+    public GameObject heightIncreasePanel;
+    public GameObject brighterSpotLightPanel;
     
     public void speedIncrease()
     {
@@ -31,7 +39,21 @@ public class ShopMenu : MonoBehaviour
             gameManager.playerCreditsText.text = gameManager.playerCredits.ToString();
 
             playerMovement.jumpForce += 2;
+            
+            heightIncreasePanel.SetActive(false);
+        }
+    }
 
+    public void spotLightIncrease()
+    {
+        if (gameManager.playerCredits >= widerFovCost)
+        {
+            gameManager.playerCredits -= widerFovCost;
+            gameManager.playerCreditsText.text = gameManager.playerCredits.ToString();
+
+            playerSpotlight.pointLightOuterRadius = 10f;
+            
+            brighterSpotLightPanel.SetActive(false);
         }
     }
 }
